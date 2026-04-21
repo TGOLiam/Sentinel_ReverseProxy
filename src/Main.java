be200b6eeb5e16f435741919fdaf614ea27661f6
Liam <yamyamm1424@gmail.com>
2026-04-21 18:20:38 +0800

    Added basic HTTP Request Listener

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        RequestListener requestListener = new RequestListener(8090);
+        requestListener.t_start();
+    }
+}
---
+++ src/RequestListener.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RequestListener {
+    private int PORT;
+    private ServerSocket serverSocket;
+
+    public RequestListener(final int port)
+    {
+        this.PORT = port;
+
+        try {
+            serverSocket = new ServerSocket(this.PORT);
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getPORT()
+    {
+        return PORT;
+    }
+
+    public void start(){
+        System.out.println("Server running at port: " + PORT);
+
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+        Socket socket;
+        while(true)
+        {
+            try{
+                socket = serverSocket.accept();
+                Socket input_socket = socket;
+                executor.submit(
+                        () -> handle_test(input_socket)
+                );
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void parseHTTP()
+    {
+
+    }
+
+    private void handle_test(Socket socket)
+    {
+        try (socket){
+            String clientIP = socket.getInetAddress().getHostAddress();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String msg = in.readLine();
+            System.out.printf("[%s] %s\n", clientIP, msg);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
